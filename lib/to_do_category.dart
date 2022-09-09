fb340308e31a506770d7acfa2bb22229dadf952a
Ted Bjurlin <teddybjurlin@gmail.com>
2022-09-09 12:27:09 -0500

    *not part of project submission this has just been bothering me* moved value listenable builder onto ok button from cancel button in add item popup

--- lib/to_do_category.dart
+++ lib/to_do_category.dart
@@ -113,35 +113,35 @@ class _ToDoCategoryState extends State<ToDoCategory> {
                     const InputDecoration(hintText: "type something here"),
               ),
             actions: <Widget>[
-              ElevatedButton(
-                key: const Key("OKButton"),
-                style: yesStyle,
-                child: const Text('OK'),
-                onPressed: () {
-                  setState(() {
-                    _handleNewItem(valueText, widget.category);
-                    Navigator.pop(context);
-                  });
-                },
-              ),
-
-              // https://stackoverflow.com/questions/52468987/how-to-turn-disabled-button-into-enabled-button-depending-on-conditions
               ValueListenableBuilder<TextEditingValue>(
                 valueListenable: _inputController,
                 builder: (context, value, child) {
                   return ElevatedButton(
-                    key: const Key("CancelButton"),
-                    style: noStyle,
+                    key: const Key("OKButton"),
+                    style: yesStyle,
+                    child: const Text('OK'),
                     onPressed: value.text.isNotEmpty
-                        ? () {
-                            setState(() {
-                              Navigator.pop(context);
-                            });
-                          }
-                        : null,
-                    child: const Text('Cancel'),
+                      ? () {
+                          setState(() {
+                            _handleNewItem(valueText, widget.category);
+                            Navigator.pop(context);
+                          });
+                        }
+                      : null,
                   );
                 },
+              ),
+
+              // https://stackoverflow.com/questions/52468987/how-to-turn-disabled-button-into-enabled-button-depending-on-conditions
+              ElevatedButton(
+                key: const Key("CancelButton"),
+                style: noStyle,
+                onPressed: () {
+                  setState(() {
+                    Navigator.pop(context);
+                  });
+                },
+                child: const Text('Cancel'),
               ),
             ],
           );
