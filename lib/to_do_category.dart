18dd7731f48eedbac5473a45b7876c5296cc9874
Garrett <colliergc@hendrix.edu>
2022-11-07 20:42:07 -0600

    Add const to remove warnings

--- lib/to_do_category.dart
+++ lib/to_do_category.dart
@@ -2,8 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:to_dont_list/to_do_items.dart';
 
 class Category {
-  Category({required this.items,
-  required this.name});
+  Category({required this.items, required this.name});
 
   List<Item> items;
 
@@ -12,20 +11,19 @@ class Category {
   List<Item> getItems() {
     return items;
   }
-
 }
 
-typedef ToDoCategoryChangedCallback = Function(Category category, bool complete);
+typedef ToDoCategoryChangedCallback = Function(
+    Category category, bool complete);
 typedef ToDoCategoryRemovedCallback = Function(Category category);
 typedef ToDoNewItemCallback = Function(Category category);
 
 class ToDoCategory extends StatefulWidget {
   ToDoCategory(
-    {required this.category,
-    required this.onDeleteCategory,
-    required this.itemSet,
-    super.key}
-  );
+      {required this.category,
+      required this.onDeleteCategory,
+      required this.itemSet,
+      super.key});
 
   final Category category;
   final ToDoCategoryRemovedCallback onDeleteCategory;
@@ -41,10 +39,9 @@ class _ToDoCategoryState extends State<ToDoCategory> {
   int _percentComplete = 0;
 
   void _adjustPercentage() {
-
     int numTrue = 0;
-    _percentComplete = (widget.itemSet.length * 100) ~/ (widget.category.items.length);
-
+    _percentComplete =
+        (widget.itemSet.length * 100) ~/ (widget.category.items.length);
   }
 
   void _handleDeleteItem(Item item) {
@@ -74,7 +71,6 @@ class _ToDoCategoryState extends State<ToDoCategory> {
       // The framework then calls build, below,
       // which updates the visual appearance of the app.
 
-
       widget.category.items.remove(item);
       if (!completed) {
         print("Completing");
@@ -90,9 +86,9 @@ class _ToDoCategoryState extends State<ToDoCategory> {
   }
 
   final ButtonStyle yesStyle = ElevatedButton.styleFrom(
-      textStyle: const TextStyle(fontSize: 20), primary: Colors.green);
+      textStyle: const TextStyle(fontSize: 20), backgroundColor: Colors.green);
   final ButtonStyle noStyle = ElevatedButton.styleFrom(
-      textStyle: const TextStyle(fontSize: 20), primary: Colors.red);
+      textStyle: const TextStyle(fontSize: 20), backgroundColor: Colors.red);
 
   Future<void> _displayTextInputDialog(BuildContext context) async {
     print("Loading Dialog");
@@ -101,17 +97,16 @@ class _ToDoCategoryState extends State<ToDoCategory> {
         builder: (context) {
           return AlertDialog(
             title: const Text('Item To Add'),
-            content: 
-              TextField(
-                onChanged: (value) {
-                  setState(() {
-                    valueText = value;
-                  });
-                },
-                controller: _inputController,
-                decoration:
-                    const InputDecoration(hintText: "type something here"),
-              ),
+            content: TextField(
+              onChanged: (value) {
+                setState(() {
+                  valueText = value;
+                });
+              },
+              controller: _inputController,
+              decoration:
+                  const InputDecoration(hintText: "type something here"),
+            ),
             actions: <Widget>[
               ValueListenableBuilder<TextEditingValue>(
                 valueListenable: _inputController,
@@ -119,15 +114,15 @@ class _ToDoCategoryState extends State<ToDoCategory> {
                   return ElevatedButton(
                     key: const Key("OKButton"),
                     style: yesStyle,
-                    child: const Text('OK'),
                     onPressed: value.text.isNotEmpty
-                      ? () {
-                          setState(() {
-                            _handleNewItem(valueText, widget.category);
-                            Navigator.pop(context);
-                          });
-                        }
-                      : null,
+                        ? () {
+                            setState(() {
+                              _handleNewItem(valueText, widget.category);
+                              Navigator.pop(context);
+                            });
+                          }
+                        : null,
+                    child: const Text('OK'),
                   );
                 },
               ),
@@ -152,56 +147,49 @@ class _ToDoCategoryState extends State<ToDoCategory> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return SizedBox(
       height: 200,
       child: Column(
         children: [
-            Row(
-              children: [ 
-                const SizedBox(
-                  width: 20
+          Row(
+            children: [
+              const SizedBox(width: 20),
+              Flexible(
+                fit: FlexFit.tight,
+                child: Text(
+                  widget.category.name,
+                  style: null,
                 ),
-                Flexible(
-                  fit: FlexFit.tight,
-                  child: Text(
-                    widget.category.name,
-                    style: null,
-                  ),
+              ),
+              const Spacer(),
+              Expanded(
+                child: Text(
+                  "$_percentComplete%",
+                  style: null,
                 ),
-                const Spacer(
+              ),
+              const SizedBox(width: 20),
+              Flexible(
+                fit: FlexFit.tight,
+                child: ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      _displayTextInputDialog(context);
+                    });
+                  },
+                  onLongPress: widget.itemSet.containsAll(widget.category.items)
+                      ? () {
+                          setState(() {
+                            widget.onDeleteCategory(widget.category);
+                          });
+                        }
+                      : null,
+                  child: const Icon(Icons.add),
                 ),
-                Expanded(
-                  child: Text(
-                    "$_percentComplete%",
-                    style: null,
-                  ),
-                ),
-                const SizedBox(
-                  width: 20
-                ),
-                Flexible(
-                  fit: FlexFit.tight,
-                  child: ElevatedButton(
-                    onPressed: () {
-                        setState(() {
-                        _displayTextInputDialog(context);
-                      });
-                    },
-                    onLongPress: widget.itemSet.containsAll(widget.category.items)
-                    ? () {
-                      setState(() {
-                        widget.onDeleteCategory(widget.category);
-                      });
-                    }
-                    : null,
-                    child: const Icon(Icons.add),
-                  ),
-                ),
-                const SizedBox(
-                  width: 20
-                )
-              ],
-            ),
+              ),
+              const SizedBox(width: 20)
+            ],
+          ),
           Flexible(
             child: ListView(
               children: widget.category.items.map((item) {
